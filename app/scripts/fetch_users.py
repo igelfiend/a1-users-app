c4aa9abb2af574c748e26fb535836ae3e28287bc
Ivan Krupin <igelfiend@protonmail.com>
2023-03-15 22:33:03 +0100

    delete redundant code, add fancy logs

--- app/scripts/fetch_users.py
+++ app/scripts/fetch_users.py
@@ -2,80 +2,31 @@ import csv
 import itertools
 import math
 import sys
+import logging
 
 import aiohttp
 import asyncio
 from tenacity import retry, wait_fixed, stop_after_attempt
 
+from app.base import Session
+from app.models import User as DbUser, UserLocation as DbUserLocation
 from app.pydantic_models.user import (
     RequestUser as PdUser,
     UserLocation as PdUserLocation,
 )
 from app.pydantic_models.user_external import ExternalUserResponse, ExternalUser
-from app.base import Session
-from app.models import User as DbUser, UserLocation as DbUserLocation
 
-
-response_mock = """
-{
-    "results": [
-        {
-            "gender": "male",
-            "name": {"title": "Mr", "first": "Janko", "last": "Dokić"},
-            "location": {
-                "street": {"number": 5103, "name": "Vinogradarska"},
-                "city": "Sečanj",
-                "state": "Jablanica",
-                "country": "Serbia",
-                "postcode": 48983,
-                "coordinates": {"latitude": "63.3861", "longitude": "-18.8406"},
-                "timezone": {"offset": "-3:30", "description": "Newfoundland"}
-            },
-            "email": "janko.dokic@example.com",
-            "login": {
-                "uuid": "5258eb16-1033-4c4f-8303-adb138a3afc0",
-                "username": "angryelephant683",
-                "password": "chilli",
-                "salt": "hampUp9q",
-                "md5": "be064c3a2557a605978ed2077b220f35",
-                "sha1": "5b48b863a45d91a74d89ccb31599852eda5e8704",
-                "sha256": "372ef0d7e52e0f0262b4e24c0a39549e0750fae27b5d9336bf943ead7b576d9b"
-            },
-            "dob": {"date": "1993-08-27T05:36:54.602Z", "age": 29},
-            "registered": {"date": "2016-09-17T17:25:19.177Z", "age": 6},
-            "phone": "015-6269-704",
-            "cell": "063-3455-035",
-            "id": {"name": "SID", "value": "426656732"},
-            "picture": {
-                "large": "https://randomuser.me/api/portraits/men/9.jpg",
-                "medium": "https://randomuser.me/api/portraits/med/men/9.jpg",
-                "thumbnail": "https://randomuser.me/api/portraits/thumb/men/9.jpg"
-            },
-            "nat": "RS"
-        }
-    ],
-    "info": {"seed": "2323a41007d4dd6f", "results": 1, "page": 1, "version": "1.4"}
-}
-"""
+logging.basicConfig(level=logging.INFO, format="%(name)s:%(levelname)s: %(message)s")
+logger = logging.getLogger("fetch_users")
 
 MAX_RETRY_COUNT = 5
 RETRY_WAIT = 1
 
-MOCK = False
-
 
 @retry(wait=wait_fixed(RETRY_WAIT), stop=stop_after_attempt(MAX_RETRY_COUNT))
 async def get_external_user_data_from_url(url: str, session: aiohttp.ClientSession):
-    if MOCK:
-        from copy import copy
-
-        return copy(response_mock)
-    else:
-        async with session.get(url, raise_for_status=True) as response:
-            data = await response.text()
-            print(f"status: {response.status}")
-            print(f"reason: {response.reason}")
-            return data
+    async with session.get(url, raise_for_status=True) as response:
+        return await response.text()
 
 
 async def fetch_user_data(url: str, users_count: int) -> dict:
@@ -150,7 +101,11 @@ def convert_external_user_to_local(
 if __name__ == "__main__":
     url = "https://randomuser.me/api/"
     users_count = sys.argv[1] if len(sys.argv) > 1 else 10
-    workers_count = sys.argv[2] if len(sys.argv) > 2 else 1
+    workers_count = sys.argv[2] if len(sys.argv) > 2 else 5
+
+    logger.info(
+        f"start fetching {users_count} users from {url} with {workers_count} workers"
+    )
     ext_users = asyncio.run(
         get_external_users_data(
             url=url,
@@ -158,6 +113,7 @@ if __name__ == "__main__":
             workers_count=workers_count,
         ),
     )
+    logger.info(f"fetched users data")
 
     nationality_map = get_nationality_names("./app/static/demonyms.csv")
     users = convert_external_user_to_local(
@@ -165,12 +121,11 @@ if __name__ == "__main__":
         nationality_map=nationality_map,
     )
 
-    print(f"received and parsed {len(list(users))} users")
+    logger.info("pushing user into the db")
 
     with Session() as session, session.begin():
         db_users = []
         for user in users:
-            print(f"user: {user.dict()}")
             user_location = DbUserLocation(**user.location.dict(exclude={"id"}))
             session.add(user_location)
             db_user = DbUser(
@@ -182,7 +137,8 @@ if __name__ == "__main__":
                     }
                 ),
             )
-            session.add(db_user)
             db_users.append(db_user)
+        session.add_all(db_users)
+        logger.info(f"pushing {len(db_users)} users into local database")
 
-    print(f"Database updated")
+    logger.info("fetch complete")
