07bdc9139d79c3ffadfb23dc21f3e3a439b8bc72
Ivan Krupin <igelfiend@protonmail.com>
2023-03-16 16:27:47 +0100

    delete reundant mixins

--- app/models/mixins.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Column, DateTime, func
-
-
-class TimestampedModelMixin():
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now())
